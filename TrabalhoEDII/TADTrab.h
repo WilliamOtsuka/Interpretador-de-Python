08da27bfe243576a1e14e9114e6f5704ad72337f
unknown <william-ots@hotmail.com>
2024-09-06 19:56:26 -0300

    clean code

--- TrabalhoEDII/TADTrab.h
+++ TrabalhoEDII/TADTrab.h
@@ -5,22 +5,22 @@ struct TpCode {
 }; typedef struct TpCode TpCode;
 
 struct TpLista {
-	struct TpLista *prox,*ant;
+	struct TpLista *prox, *ant;
 	TpCode *tokens;
 }; typedef struct TpLista TpLista;
 
-void CriaL (TpLista**nova){
-	*nova=(TpLista*)malloc(sizeof(TpLista));
-	(*nova)->ant=NULL;
-	(*nova)->prox=NULL;
-	(*nova)->tokens=NULL;
+void CriaL (TpLista **nova){
+	*nova = (TpLista*)malloc(sizeof(TpLista));
+	(*nova)->ant = NULL;
+	(*nova)->prox = NULL;
+	(*nova)->tokens = NULL;
 }
 
-void InsereL(TpLista** pProgram){
-	TpLista *aux,*nova;
+void InsereL(TpLista **pProgram){
+	TpLista *aux, *nova;
 	CriaL(&nova);
-	if(*pProgram==NULL)
-		*pProgram=nova;
+	if(*pProgram == NULL)
+		*pProgram = nova;
 	
 	else
 	{
@@ -34,65 +34,70 @@ void InsereL(TpLista** pProgram){
 	}
 }
 
-void CriaT (TpCode**nova,char token[15]){
-	*nova=(TpCode*)malloc(sizeof(TpCode));
-	strcpy((*nova)->token,token);
-	(*nova)->prox=NULL;
+void CriaT (TpCode **nova,char token[15]){
+	*nova = (TpCode*)malloc(sizeof(TpCode));
+	strcpy((*nova)->token, token);
+	(*nova)->prox = NULL;
 }
 
-void InsereT(TpLista**pProgram,TpCode*nova){
-	TpLista *auxL=*pProgram;
+void InsereT(TpLista **pProgram, TpCode *nova){
+	TpLista *auxL = *pProgram;
 	TpCode *aux;
 	
-	while(auxL->prox!=NULL)
-		auxL=auxL->prox;
-	if(auxL->tokens==NULL)
-		auxL->tokens=nova;
+	while(auxL->prox != NULL)
+		auxL = auxL->prox;
+
+	if(auxL->tokens == NULL)
+		auxL->tokens = nova;
+
 	else
 	{
-		aux=auxL->tokens;
-		while(aux->prox!=NULL)
-			aux=aux->prox;
-		aux->prox=nova;
+		aux = auxL->tokens;
+		while(aux->prox != NULL)
+			aux = aux->prox;
+		aux->prox = nova;
 	}
 }
 
-char VerifaVazia(TpLista*pProgram){
-	TpLista *aux=pProgram;
-	while(aux->prox!=NULL)
-		aux=aux->prox;
-	if(aux->tokens==NULL)
+char VerifaVazia(TpLista *pProgram){
+	TpLista *aux = pProgram;
+	while(aux->prox != NULL)
+		aux = aux->prox;
+
+	if(aux->tokens == NULL)
 		return 1;
+
 	else
 		return 0;
 }
 
-void exibe(TpLista*pProgram){
-	TpLista *aux=pProgram;
+void exibe(TpLista *pProgram){
+	TpLista *aux = pProgram;
 	TpCode *auxT;
-	while(aux->prox!=NULL)
+
+	while(aux->prox != NULL)
 	{
-		auxT=aux->tokens;
-		while(auxT!=NULL)
+		auxT = aux->tokens;
+		while(auxT != NULL)
 		{
 			printf("%s ",auxT->token);
-			auxT=auxT->prox;
+			auxT = auxT->prox;
 		}
 		printf("\n");
-		aux=aux->prox;
+		aux = aux->prox;
 	}
 }
 
-void CarregaL(TpLista**pProgram){
+void CarregaL(TpLista **pProgram){
 	FILE *arq;
-	int i,j;
-	char token[15],nome[15],auxS[50];
+	int i, j;
+	char token[15], nome[15], auxS[50];
 	TpCode *nova;
 	printf("Digite o nome do arquivo: ");
-	scanf("%s",nome);
-	arq=fopen(nome,"r");
+	scanf("%s", nome);
+	arq = fopen(nome, "r");
 
-	if(arq==NULL)
+	if(arq == NULL)
 	{
 		printf("Erro ao abrir o arquivo\n");
 	}
@@ -100,17 +105,17 @@ void CarregaL(TpLista**pProgram){
 	{
 		while(!feof(arq))
 		{
-			fgets(auxS,50,arq);
+			fgets(auxS, 50, arq);
 			InsereL(&pProgram);
-			for(i=0;i<strlen(auxS);i++)
+			for(i = 0; i < strlen(auxS); i++)
 			{
-				for(j=0;auxS[i]!=' ' && auxS[i]!='\n';j++,i++)
+				for(j = 0; auxS[i] != ' ' && auxS[i] != '\n'; j++, i++)
 				{
-					token[j]=auxS[i];
+					token[j] = auxS[i];
 				}
-				token[j]='\0';
-				CriaT(&nova,token);
-				InsereT(pProgram,nova);
+				token[j] = '\0';
+				CriaT(&nova, token);
+				InsereT(pProgram, nova);
 			}
 		}
 	}
